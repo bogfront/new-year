667b8ff4f30a34102edd1c943bdba76e3954c5bd
Konovchenko Bogdan <konovchenkobv@gmail.com>
2018-12-19 11:23:56 +0300

    Сверстал

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container">
+            <Cover :host="host"/>
+            <Stat :year="year"/>
+            <hr>
+            <Stat :month="month"/>
+            <hr>
+            <Stat :time="time"/>
+            <hr>
+            <Stat :day="day"/>
+            <Stat :channel="channel"/>
+            <Footer/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import Cover from "./components/Cover";
+    import Stat from "./components/Stat";
+    import Footer from "./components/Footer";
+
+    export default {
+        name: 'app',
+        components: {
+            Footer,
+            Stat,
+            Cover
+
+        },
+        data() {
+            return ({
+                host: {
+                    name: 'Хостнейм',
+                    logo: '/img/logohost.png',
+                    eventsCovers: ['/img/cover1.png', '/img/cover2.png']
+                },
+                year: {
+                    events: 114,
+                    tickets: 2113444,
+                    money: 203113444
+                },
+                month: 'Июль',
+                time: '17:00',
+                day: 'Пятница',
+                channel: 'Виджет, касса и билетный стол'
+            });
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    button {
+        border: none;
+        box-shadow: none;
+    }
+
+    hr {
+        border: none;
+        border-bottom: 1px solid #e5e5e5;
+        width: 80%;
+    }
+
+    .container {
+        width: 1200px;
+        margin: 280px auto;
+        background: #fff;
+        border-radius: 10px;
+        box-shadow: 0 30px 100px rgba(0, 0, 0, 0.3);
+    }
+
+    .title {
+        font-size: 40px;
+        line-height: 50px;
+        font-weight: 700;
+        color: #111;
+        margin-bottom: 30px;
+    }
+    .subtitle {
+        opacity: 0.8;
+        color: #000000;
+        font-size: 22px;
+        font-weight: 400;
+        line-height: 30px;
+    }
+
+    .btn {
+        color: #fff;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 50px;
+        border-radius: 25px;
+        background: #466eff;
+        padding: 0 32px;
+        cursor: pointer;
+        transition: background .2s;
+    }
+    .btn:hover {
+        background: #0b4fff;
+    }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="footer">
+        <div class="icon"></div>
+        <p class="subtitle">Спасибо, что вы с нами! Мы благодарны за доверие и будем<br>
+            делать сервис еще лучше. Вместе нас ждут великие дела.</p>
+        <button class="btn">Подробная статистика</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    .footer {
+        text-align: center;
+        padding:  150px 30px;
+        border-radius: 0 0 10px 10px;
+    }
+
+    .icon {
+        margin: 0 auto 30px;
+        width: 47px;
+        height: 60px;
+        background: url('../assets/christmas-tree.png') center no-repeat;
+        background-size: cover;
+    }
+
+    .subtitle {
+        margin-bottom: 40px;
+    }
+</style>
